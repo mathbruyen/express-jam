3f1e924bad38462328b453e2807a1fa9c08b8c76
Mathieu Bruyen <code@mais-h.eu>
2012-09-02 16:50:23 +0200

    Handles properly packageDir statement

--- src/configure.js
+++ src/configure.js
@@ -1,13 +1,19 @@
 // Copyright (c) 2012 Mathieu Bruyen
 // See the file license.txt for copying permission.
 
-var express   = require('express');//TODO use connect
+var connect   = require('connect');
 var filed     = require('filed');
 var fs        = require('fs');
 var crypto    = require('crypto');
-var reader    = require('npm-package-reader');
-var async     = require('async');
 var npmLoader = require('npm');
+
+// Polyfill for Regexp.quote from
+// http://stackoverflow.com/questions/494035/how-do-you-pass-a-variable-to-a-regular-expression-javascript/494122#494122
+if (!RegExp.quote) {
+  RegExp.quote = function(str) {
+    return str.replace(/([.?*+\^$\[\]\\(){}|-])/g, "\\$1");
+  };
+}
 
 function addSetting(app, setting, key, value) {
   var c = app.settings[setting] || {};
@@ -15,13 +21,12 @@ function addSetting(app, setting, key, value) {
   app.set(setting, c);
 }
 
-function development(app, done) {
+function development(app, config, done) {
   var main = '/require.js';
   fs.exists(config.rootDir + config.jamDir + main, function(exists) {
     if (exists) {
-      addSetting(app, 'view options', config.jamViewKey, config.baseUrl + main);
-      //TODO honor baseUrl in regular expression
-      app.get(/^\/jam\/.*\.js$/, express.static(config.rootDir));
+      addSetting(app, 'view options', config.jamViewKey, config.jamDir + main);
+      app.get(new RegExp('^' + RegExp.quote(config.jamDir) + '\\/.*\\.js$'), connect.static(config.rootDir));
       done();
     } else {
       done('Jam is not installed');
@@ -29,7 +34,7 @@ function development(app, done) {
   });
 }
 
-function production(app, done) {
+function production(app, config, done) {
   var catalog = '/catalog.js';
   fs.exists(config.rootDir + config.jamDir + catalog, function(exists) {
     if (exists) {
@@ -40,7 +45,7 @@ function production(app, done) {
       });
       s.on('end', function() {
         var hash = shasum.digest('hex');
-        var uri = config.baseUrl + '/catalog_' + hash + '.js';
+        var uri = config.jamDir + '/catalog_' + hash + '.js';
         addSetting(app, 'view options', config.jamViewKey, uri);
         app.get(uri, function (req, resp) {
           req.pipe(filed(config.rootDir + config.jamDir + catalog)).pipe(resp);
@@ -54,33 +59,39 @@ function production(app, done) {
 }
 
 module.exports = function(app, done) {
-  async.parallel([npmLoader.load, reader.on], function(err, results) {
-    var rootDir = results[0].prefix;
-    var jamDir =  '/jam';
-    var baseUrl = '/jam';
-    if (results[1].jam ) {
-      if (results[1].jam.packageDir) {
-        jamDir = results[1].jam.packageDir;
-      }
-      if (results[1].jam.baseUrl) {
-        baseUrl = results[1].jam.baseUrl;
-      }
-    }
-    var jamViewKey = process.env.JAM_VIEW_KEY || 'jam_uri';
+  npmLoader.load(function(err, npm) {
+    var root = npm.prefix;
+    fs.readFile(root + '/package.json', 'utf-8', function(err, content) {
+      try {
+        var pack = JSON.parse(content);
+        var jamDir = '/jam';
+        if (pack.jam ) {
+          if (pack.jam.packageDir) {
+            jamDir = '/' + pack.jam.packageDir;
+          }
+        }
+        var jamViewKey = process.env.JAM_VIEW_KEY || 'jam_uri';
 
-    //TODO accept configuration as parameter
-    var config = {
-      'rootDir'    : rootDir,
-      'jamDir'     : jamDir,
-      'baseUrl'    : baseUrl,
-      'jamViewKey' : jamViewKey
-    };
+        //"postinstall": ""
 
-    app.configure('development', function() {
-      development(app, done);
-    });
-    app.configure('production', function() {
-      production(app, done);
+        //TODO accept configuration as parameter
+        var config = {
+          'rootDir'    : root,
+          'jamDir'     : jamDir,
+          'jamViewKey' : jamViewKey
+        };
+
+        app.configure('development', function() {
+          development(app, config, done);
+        });
+        app.configure('production', function() {
+          production(app, config, done);
+        });
+      } catch(ex) {
+        process.nextTick(function () {
+          done(ex);
+        });
+      }
     });
   });
 };
